42aa2c3350bedc9b839b784ac90e8256f458a920
Lawrence Dumaguing <os2nlinux@gmail.com>
2021-12-09 16:46:54 -0500

    2021.12a

--- 6502/get38.c
+++ 6502/get38.c
@@ -27,29 +27,41 @@ int bcdToDec(char b) { return (b/16)*10 + (b%16); }
 
 int main(){
    int file;
-   printf("Starting the DS3231 test application\n");
+   printf("Starting...\n");
    if((file=open("/dev/i2c-1", O_RDWR)) < 0){
       perror("failed to open the bus\n");
       return 1;
    }
-   if(ioctl(file, I2C_SLAVE, 0x68) < 0){
+
+   if(ioctl(file, I2C_SLAVE, 0x38) < 0) {
       perror("Failed to connect to the sensor\n");
       return 1;
    }
+
+/*   
    char writeBuffer[1] = {0x00};
    if(write(file, writeBuffer, 1)!=1){
       perror("Failed to reset the read address\n");
       return 1;
    }
-   char buf[BUFFER_SIZE];
-   if(read(file, buf, BUFFER_SIZE)!=BUFFER_SIZE){
+*/
+
+   
+   char buf[3];
+   if(read(file, buf, 3) == -1){
       perror("Failed to read in the buffer\n");
       return 1;
    }
+   printf("%d\n", atoi(buf[0]));
+
+   /*
    printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]),
          bcdToDec(buf[1]), bcdToDec(buf[0]));
    float temperature = buf[0x11] + ((buf[0x12]>>6)*0.25);
    printf("The temperature is %.2f°C\n", temperature);
+   */
+
+  // printf("%d\n", buf);
    close(file);
    return 0;
 }
