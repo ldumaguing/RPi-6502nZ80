59b697ee3b9084928364242f06176e8af91868f5
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-01-03 19:27:51 -0500

    2022.01a

--- 65816/MyComputer/Test-01/test.c
+++ 65816/MyComputer/Test-01/test.c
@@ -2,5 +2,9 @@
 #include <math.h>
 
 void main() {
-    printf ( "%f",pow ( 2,2 ) );
+    printf ( "%f\n",pow ( 2,2 ) );
+    int x = 1;
+    printf("%d\n", x);
+    printf("%d\n", x <<= 1);
+    printf("%d\n", x <<= 1);
 }
