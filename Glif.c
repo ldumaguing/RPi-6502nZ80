0df8ccd7d65438d44a5ae3068afd4a164c9366d2
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-01-20 15:44:49 -0500

    Update Glif.c

--- Glif.c
+++ Glif.c
@@ -4,11 +4,34 @@
 
 int main() {
     setlocale(LC_CTYPE, "");
-    for (int i=5793; i<=5880; i++){
-    wprintf(L"%lc", (wchar_t)i);
+    for (int i=5793; i<=5880; i++) {
+        wprintf(L"%lc", (wchar_t)i);
     }
     wprintf(L"\n");
     
+    for (int i=8593; i<=8597; i++) {
+        wprintf(L"%lc", (wchar_t)i);
+    }
+    wprintf(L"\n");
     
-}
+    for (int i=8602; i<=8616; i++) {
+        wprintf(L"%lc", (wchar_t)i);
+    }
+    wprintf(L"\n");
+    
+    for (int i=8619; i<=8703; i++) {
+        wprintf(L"%lc", (wchar_t)i);
+    }
+    wprintf(L"\n");
+    
+    for (int i=8853; i<=8865; i++) {
+        wprintf(L"%lc", (wchar_t)i);
+    }
+    wprintf(L"\n");
+    
+    for (int i=8865; i<=10000; i++) {
+        wprintf(L"%lc", (wchar_t)i);
+    }
+    wprintf(L"\n");
 
+}
