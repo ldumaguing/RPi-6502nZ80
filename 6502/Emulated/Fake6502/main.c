d9a01892c3df0e5f8ffc02b1fdd0cc019dd24f52
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-04-06 11:52:57 -0400

    Update main.c

--- 6502/Emulated/Fake6502/main.c
+++ 6502/Emulated/Fake6502/main.c
@@ -18,9 +18,9 @@ uint8_t mem_read ( context_t *c, uint16_t addr ) {
 int main ( int argc, char *argv[] ) {
     context_t c;
     uint8_t *ROM = &c.mem[0xc000];
-   
+
     FILE* fp;
-    if (argc < 2)
+    if ( argc < 2 )
         fp = fopen ( "rom.bin", "rb" );
     else
         fp = fopen ( argv[1], "rb" );
@@ -31,14 +31,21 @@ int main ( int argc, char *argv[] ) {
     c.mem[0xfffc] = 0x00;
     c.mem[0xfffd] = 0xc0;
 
-
+    c.mem[0x0012] = 13;
 
 
     reset6502 ( &c );
     printf ( "start\n" );
+    int ticks = 0;
     for ( ;; ) {
-        printf ( "%x.%x.%x.%x %x.%x.%x\n", c.a, c.x, c.y, c.flags, c.pc, c.opcode, c.ea );
+        ticks = c.clockticks;
+        printf ( "%x ", c.pc );
         step ( &c );
+        printf ( "%02x", c.opcode );
+        // printf ( "%x.%x.%x.%x %x.%x.%x", c.a, c.x, c.y, c.flags, c.pc, c.opcode, c.ea );
+
+        printf ( " %d\n", c.clockticks - ticks );
+        ticks = 0;
     }
 
 
