7d4c60857d86054ef8aa13c15ff574247b1a435a
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-04-07 17:07:55 -0400

    Update main.c

--- 6502/Emulated/Fake6502/main.c
+++ 6502/Emulated/Fake6502/main.c
@@ -17,7 +17,7 @@ uint8_t mem_read ( context_t *c, uint16_t addr ) {
 // **************************************************************************************
 int main ( int argc, char *argv[] ) {
     context_t c;
-    uint8_t *ROM = &c.mem[0xc000];
+    uint8_t *ROM = &c.mem[0x0200];
 
     FILE* fp;
     if ( argc < 2 )
@@ -29,32 +29,40 @@ int main ( int argc, char *argv[] ) {
 
     // reset vector
     c.mem[0xfffc] = 0x00;
-    c.mem[0xfffd] = 0xc0;
+    c.mem[0xfffd] = 0x02;
 
-    c.mem[0x0012] = 13;
-
+    // ************************************************************************ PPU
+    // ********** HexTileWindow
+    c.mem[0x9f00] = 0;   // x
+    c.mem[0x9f01] = 0;   // y
+    c.mem[0x9f02] = 0;   // width
+    c.mem[0x9f03] = 0;   // height
+    c.mem[0x9f04] = 0;   // Signals:
+                         //    0 - PPU waiting
+                         //    1 - Hey PPU, refresh.
+                         //    2 - PPU refreshing.
 
     reset6502 ( &c );
     printf ( "start\n" );
     for ( ;; ) {
-        printf ( "%x ", c.pc );
+        printf ( "%04x ", c.pc );
         step ( &c );
-        printf ( "%02x", c.opcode );
+        printf ( "%02x% 20x\n", c.opcode, c.mem[0x9f00] );
         // printf ( "%x.%x.%x.%x %x.%x.%x", c.a, c.x, c.y, c.flags, c.pc, c.opcode, c.ea );
 
-        printf ( " %d\n", c.clockticks );
-        c.clockticks = 0;
-        
+       // printf ( " %d\n", c.clockticks );
+      //  c.clockticks = 0;
+
         /*
             while ( c.clockticks > 0 ) {
             printf ( ".\n" );
             c.clockticks--;
         }
         */
-        
-        
-        
-        
+
+
+
+
     }
 
 
